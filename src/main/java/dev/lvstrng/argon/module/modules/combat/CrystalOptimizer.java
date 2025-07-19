75af02e81f1cd447d3192f283df6b8a4db1431f7
WibuAkut820 <zmc511721@gmail.com>
2025-07-19 11:57:47 +0800

    Update CrystalOptimizer.java

--- src/main/java/dev/lvstrng/argon/module/modules/combat/CrystalOptimizer.java
+++ src/main/java/dev/lvstrng/argon/module/modules/combat/CrystalOptimizer.java
@@ -21,8 +21,8 @@ import net.minecraft.util.math.Vec3d;
 
 public final class CrystalOptimizer extends Module implements PacketSendListener {
 	public CrystalOptimizer() {
-		super(EncryptedString.of("Crystal Optimizer"),
-				EncryptedString.of("Makes your crystals disappear faster client-side so you can place crystals faster"),
+		super(EncryptedString.of("optimize dick crystal"),
+				EncryptedString.of("crystal optimizer"),
 				-1,
 				Category.COMBAT);
 	}
