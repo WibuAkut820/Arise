3ec70ad6e3a820d103f6b431da1355355d04f55b
WibuAkut820 <zmc511721@gmail.com>
2025-07-19 12:04:17 +0800

    Update Category.java

--- src/main/java/dev/lvstrng/argon/module/Category.java
+++ src/main/java/dev/lvstrng/argon/module/Category.java
@@ -3,10 +3,10 @@ package dev.lvstrng.argon.module;
 import dev.lvstrng.argon.utils.EncryptedString;
 
 public enum Category {
-	COMBAT(EncryptedString.of("Combat")),
-	MISC(EncryptedString.of("Misc")),
-	RENDER(EncryptedString.of("Render")),
-	CLIENT(EncryptedString.of("Client"));
+	COMBAT(EncryptedString.of("Crystal Module")),
+	MISC(EncryptedString.of("ETC")),
+	RENDER(EncryptedString.of("Aura Farming")),
+	CLIENT(EncryptedString.of("Sex"));
 	public final CharSequence name;
 
 	Category(CharSequence name) {
